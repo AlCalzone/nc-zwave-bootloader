87856ad9da141949cf7ba6d8028669014dbee13f
Dominic Griesel <d.griesel@gmx.net>
2024-11-28 13:57:43 +0100

    custom modifications

--- config/pin_config.h
+++ config/pin_config.h
@@ -74,6 +74,22 @@
 // [TIMER4]$
 
 // $[USART0]
+// USART0 RX on PA07
+#ifndef USART0_RX_PORT                          
+#define USART0_RX_PORT                           gpioPortA
+#endif
+#ifndef USART0_RX_PIN                           
+#define USART0_RX_PIN                            7
+#endif
+
+// USART0 TX on PA08
+#ifndef USART0_TX_PORT                          
+#define USART0_TX_PORT                           gpioPortA
+#endif
+#ifndef USART0_TX_PIN                           
+#define USART0_TX_PIN                            8
+#endif
+
 // [USART0]$
 
 // $[I2C1]
